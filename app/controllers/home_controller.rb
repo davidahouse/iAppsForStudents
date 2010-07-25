3428794bf62c2f5c177c5f8a58d212d3ed637f8d
David House <davidahouse@gmail.com>
2010-07-24 20:20:49 -0400

    Improvements to app edit and home view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,11 +8,14 @@ class HomeController < ApplicationController
     
     # gather up all the applications sorted by company first, then application second
     if ( params[:platform] == 'mac' )
-      @applications = Application.find(:all,:conditions => ['platform = "M"'])
+      @applications = Application.find(:all,:include => :company, :order => "companies.title,applications.title", 
+                    :conditions => ['platform = "M"'])
     elsif ( params[:platform] == 'iphone' )
-      @applications = Application.find(:all,:conditions => ['platform = "P" or platform = "U"'])
+      @applications = Application.find(:all,:include => :company, :order => "companies.title,applications.title",
+                    :conditions => ['platform = "P" or platform = "U"'])
     elsif (params[:platform] == 'ipad')
-      @applications = Application.find(:all,:conditions => ['platform = "D" or platform = "U"'])  
+      @applications = Application.find(:all,:include => :company, :order => "companies.title,applications.title",
+                    :conditions => ['platform = "D" or platform = "U"'])  
     end
   end
 
--- app/models/application.rb
+++ app/models/application.rb
@@ -1,9 +1,14 @@
 class Application < ActiveRecord::Base
   
+  belongs_to :company
+  
   has_attached_file :icon,
     :styles => { :small => "150x150>" },
     :storage => :s3,
-    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+    :bucket => ENV['S3_BUCKET'],
+    :s3_credentials => 
+        { :access_key_id => ENV['S3_ACCESSKEY'],
+          :secret_access_key => ENV['S3_SECRETKEY'] },
     :path => ":attachment/:id/:style.:extension"
   
 end
