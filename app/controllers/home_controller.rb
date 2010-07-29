49fa2ed23ffafc06ef93406132b5da099dda8bbe
David House <davidahouse@gmail.com>
2010-07-28 22:42:42 -0400

    Added countdown and stats to home view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,16 +9,44 @@ class HomeController < ApplicationController
       @platform = 'mac'
     end
     
-    # gather up all the applications sorted by company first, then application second
-    if ( @platform == 'mac' )
-      @applications = Application.find(:all,:include => :company, :order => "companies.title,applications.title", 
-                    :conditions => ['platform = "M"'])
-    elsif ( @platform == 'iphone' )
-      @applications = Application.find(:all,:include => :company, :order => "companies.title,applications.title",
+    if ENV['saleActive'] == 'YES'
+      
+      # gather up all the applications sorted by company first, then application second
+      if ( @platform == 'mac' )
+        @applications = Application.find(:all,:include => :company, :order => "companies.title,applications.title", 
+                      :conditions => ['platform = "M"'])
+      elsif ( @platform == 'iphone' )
+        @applications = Application.find(:all,:include => :company, :order => "companies.title,applications.title",
                     :conditions => ['platform = "P" or platform = "U"'])
-    elsif (@platform == 'ipad')
-      @applications = Application.find(:all,:include => :company, :order => "companies.title,applications.title",
+      elsif (@platform == 'ipad')
+        @applications = Application.find(:all,:include => :company, :order => "companies.title,applications.title",
                     :conditions => ['platform = "D" or platform = "U"'])  
+      end
+    
+    else
+      @devcount = Company.count
+      
+      @appcount = 0
+      @totaldiscount = 0.0
+      apps = Application.find(:all)
+      for a in apps
+        
+        if ( ( a.price_currency != nil ) && ( a.discount != nil ) )
+        
+          if ( a.price_currency == 'USD' )
+            @totaldiscount = @totaldiscount + a.discount
+          elsif ( a.price_currency == 'EUR' )
+            @totaldiscount = @totaldiscount + (a.discount * 1.2979)
+          else
+            @totaldiscount = @totaldiscount + (a.discount * 1.5583)
+          end
+        end
+        
+        @appcount = @appcount + 1
+      end
+      
+      @totaldiscount = @totaldiscount.to_i
+      
     end
     
   end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,68 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function countdown_clock(year, month, day, hour, minute, format)
+         {
+         //I chose a div as the container for the timer, but
+         //it can be an input tag inside a form, or anything
+         //who's displayed content can be changed through
+         //client-side scripting.
+         html_code = '<div id="countdown"></div>';
+         
+         document.write(html_code);
+         
+         countdown(year, month, day, hour, minute, format);                
+         }
+         
+function countdown(year, month, day, hour, minute, format)
+         {
+         Today = new Date();
+         Todays_Year = Today.getFullYear() - 2000;
+         Todays_Month = Today.getMonth();                  
+         
+         //Convert both today's date and the target date into miliseconds.                           
+         Todays_Date = (new Date(Todays_Year, Todays_Month, Today.getDate(), 
+                                 Today.getHours(), Today.getMinutes(), Today.getSeconds())).getTime();                                 
+         Target_Date = (new Date(year, month - 1, day, hour, minute, 00)).getTime();                  
+         
+         //Find their difference, and convert that into seconds.                  
+         Time_Left = Math.round((Target_Date - Todays_Date) / 1000);
+         
+         if(Time_Left < 0)
+            Time_Left = 0;
+         
+         switch(format)
+               {
+               case 0:
+                    //The simplest way to display the time left.
+                    document.all.countdown.innerHTML = Time_Left + ' seconds';
+                    break;
+               case 1:
+                    //More datailed.
+                    days = Math.floor(Time_Left / (60 * 60 * 24));
+                    Time_Left %= (60 * 60 * 24);
+                    hours = Math.floor(Time_Left / (60 * 60));
+                    Time_Left %= (60 * 60);
+                    minutes = Math.floor(Time_Left / 60);
+                    Time_Left %= 60;
+                    seconds = Time_Left;
+                    
+                    dps = 's'; hps = 's'; mps = 's'; sps = 's';
+                    //ps is short for plural suffix.
+                    if(days == 1) dps ='';
+                    if(hours == 1) hps ='';
+                    if(minutes == 1) mps ='';
+                    if(seconds == 1) sps ='';
+                    
+                    document.all.countdown.innerHTML = days + ' day' + dps + ' ';
+                    document.all.countdown.innerHTML += hours + ' hour' + hps + ' ';
+                    document.all.countdown.innerHTML += minutes + ' minute' + mps + ' and ';
+                    document.all.countdown.innerHTML += seconds + ' second' + sps;
+                    break;
+               default: 
+                    document.all.countdown.innerHTML = Time_Left + ' seconds';
+               }
+               
+         //Recursive call, keeps the clock ticking.
+         setTimeout('countdown(' + year + ',' + month + ',' + day + ',' + hour + ',' + minute + ',' + format + ');', 1000);
+         }
