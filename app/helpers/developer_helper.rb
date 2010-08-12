4af53c3a35844577c31c92d8d494e24494f6a2ba
David House <davidahouse@gmail.com>
2010-08-12 18:45:47 -0400

    Changed how price was displayed to include currency symbol, not the 3 letter currency code

--- app/helpers/developer_helper.rb
+++ app/helpers/developer_helper.rb
@@ -14,4 +14,17 @@ module DeveloperHelper
     return rtn
   end
   
+  def currSymbol(currency)
+  
+    if currency == "USD"
+      return "$"
+    elsif currency == "GBP"
+      return "£"
+    elsif currency == "EUR"
+      return "€"
+    else
+      return "$"
+    end
+  end
+  
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -14,4 +14,16 @@ module HomeHelper
     return rtn
   end
   
+  def currSymbol(currency)
+  
+    if currency == "USD"
+      return "$"
+    elsif currency == "GBP"
+      return "£"
+    elsif currency == "EUR"
+      return "€"
+    else
+      return "$"
+    end
+  end
 end
